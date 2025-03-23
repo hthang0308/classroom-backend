2481e9a3ca56b14e72b55b306003c9fcec341b3d
Thang Huynh <hthang0308a@gmail.com>
2025-03-23 23:40:01 +0700

    Add health check endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigurationModule } from './config/configuration.module';
 import { GroupModule } from './group/group.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigurationModule, AuthModule, GroupModule],
+  imports: [ConfigurationModule, AuthModule, GroupModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
